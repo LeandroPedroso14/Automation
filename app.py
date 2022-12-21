81287cf3d104534b3de67bf67e83a1e1f68cfbb1
Leandro Pedroso <106255930+LeandroPedroso14@users.noreply.github.com>
2022-12-21 07:22:10 -0300

    Adding comments

--- app.py
+++ app.py
@@ -11,3 +11,7 @@ pyautogui.write('123456')
 
 #clicando em entrar
 pyautogui.click(2472,173,duration=2)
+
+#Dica para cancelar o percurso automático da seta, basta 
+#elevar até o topo da tela no lado esquerdo.
+
